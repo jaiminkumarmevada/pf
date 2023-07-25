d0ec6729ca9402fcbd335a12700b5c53193ee2d2
Jaimin J. Mevada <90691437+jaiminkumarmevada@users.noreply.github.com>
2023-07-25 09:48:07 +0530

    under developement phase

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var downloadBtn = document.getElementById("newbtn");
+    downloadBtn.addEventListener("click",function() {
+      alert("For resume request at Email: jaiminkumarmevada@gmail.com"); });
+    });
